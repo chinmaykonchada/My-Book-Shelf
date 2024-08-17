aa507b63c6120204040f3d538f0df8bed7a60b33
Chinmay Konchada <chinmaykonchada@gmail.com>
2024-08-17 18:46:59 +0530

    database is created!

--- My-Book-Shelf/index.js
+++ My-Book-Shelf/index.js
@@ -1,15 +1,28 @@
 import express from 'express';
 import bp from "body-parser";
+import pg from "pg";
 
 const app=express();
 const port = 3000;
 
+const db = new pg.Client({
+    user: "postgres",
+    host: "localhost",
+    database: "My-Book-Shelf",
+    password: "chinmay@123",
+    port: 5432,
+});
+db.connect();
+
 let books=[
-    {title:"the 5 am club", timestamp:"2:4:2023", rating:5, description:"contesdfasdfadf", notes:"nogasafaf"},
-    {title:"the 5 am club", timestamp:"2:4:2023", rating:5, description:"contesdfasdfadf", notes:"nogasafaf"}
+    {title:"the 5 am club", lastedited:"2:4:2023", rating:5, description:"contesdfasdfadf", notes:"nogasafaf"},
+    {title:"the 5 am club", lastedited:"2:4:2023", rating:5, description:"contesdfasdfadf", notes:"nogasafaf"}
 ]
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
+    const result=await db.query("SELECT title, lastedited, rating, description FROM books");
+    // console.log(result);
+    books=result.rows;
     res.render("index.ejs", {allbooks: books});
 })
 
